924d464861770963d71fade6bbfabefc4d370660
Andy Weiss <andyweiss1982@gmail.com>
2018-06-19 13:32:16 -0400

    conditionally hide and show form fields

--- app/assets/javascripts/createCard.js
+++ app/assets/javascripts/createCard.js
@@ -1,19 +1,32 @@
-$(document).ready(function(){
-  $("select").change(function(){
-    if ($("#category").val("Gift")){
-      $("#company_name").attr("disabled", false);
-      $("#amount").attr("disabled", false);
-      $("#expire").attr("disabled", false);
-    }
-    else if($("#category").val("Membership")){
-      $("#company_name").attr("disabled", false);
-      $("#amount").attr("disabled", true);
-      $("#expire").attr("disabled", false);
-    }
-    else ($("#category").val("Rewards")){
-      $("#company_name").attr("disabled", false);
-      $("#amount").attr("disabled", true);
-      $("#expire").attr("disabled", true);
+$(document).on("turbolinks:load", function(){
+  $("select#card_category").change(function(){
+    if($("select#card_category").val() === "Gift"){
+      $("#card_company_name").attr("disabled", false);
+      $("#company_field").show();
+
+      $("#card_amount").attr("disabled", false);
+      $("#amount_field").show();
+
+      $("#card_expire").attr("disabled", false);
+      $("#expire_field").show();
+    }else if($("select#card_category").val() === "Membership"){
+      $("#card_company_name").attr("disabled", false);
+      $("#company_name_field").show();
+
+      $("#card_amount").attr("disabled", true);
+      $("#amount_field").hide();
+
+      $("#card_expire").attr("disabled", false);
+      $("#expire_field").show();
+    }else if($("select#card_category").val() === "Rewards"){
+      $("#card_company_name").attr("disabled", false);
+      $("#company_name_field").show();
+
+      $("#card_amount").attr("disabled", true);
+      $("#amount_field").hide();
+
+      $("#card_expire").attr("disabled", true);
+      $("#expire_field").hide();
     }
   });
 });
