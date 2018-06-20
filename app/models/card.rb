85ef9c47700e61f8aa2251053412ba3283a62afb
MvrickHntrZero <Zeronamikaze@gmail.com>
2018-06-20 13:25:11 -0400

    commit that adds an expiration checker/reworked some devise data

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,6 @@
 class Card < ApplicationRecord
+   scope :expired, -> {where("expire is not null and expire < ?", Date.today)}
+   scope :expiring_soon, -> {where("expire is not null and expire >= ? and expire < ?", Date.today, 10.days.from_now.to_date)}
+
    belongs_to :user
 end
---
+++ lib/tasks/expiration.rake
@@ -0,0 +1,7 @@
+namespace :expiration do
+  desc "Send an email to each user with an expiring card"
+  task send_emails: :environment do
+    puts "DOING SOMETHING"
+  end
+
+end
