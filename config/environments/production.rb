2966d4a9d1eb27dd819888450213c4e0614425d4
Andy Weiss <andyweiss1982@gmail.com>
2018-06-21 15:07:10 -0400

    config email for prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,17 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'card-vault.herokuapp.com', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV['EMAIL_USERNAME'],
+    password:             ENV['EMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
